ed5b84f6d311546f1601d080dc68bc717e8ad470
zarazarath <70988276+zarazarath@users.noreply.github.com>
2020-09-23 14:58:03 +0900

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -26,7 +26,7 @@ public class Tracker {
         return entries.size();
     }
 
-    public TimeEntry get(int index) {
+    public TimeEntry get(index) {
         catch (Exception e) {
 
         }
