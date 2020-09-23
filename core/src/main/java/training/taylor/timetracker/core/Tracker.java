d06bf7a16bec1017916b8dced6dca61095ce6bec
zarazarath <70988276+zarazarath@users.noreply.github.com>
2020-09-23 14:51:23 +0900

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,9 +16,9 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
-        if (true)
+        if (true){
             entries.remove(entry);
-
+        }
         entries.remove(entry);
     }
 
@@ -27,17 +27,11 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
+        catch (Exception e) {
 
         }
 
         boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
 
         return entries.get(index);
     }
